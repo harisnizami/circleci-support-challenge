fd0014225b6303aba29ed64cfdcfd66892c67438
Haris Amna Nyle <han@Hariss-MacBook-Pro.local>
2026-05-01 17:17:32 -0400

    Test failing build

--- tests/button.spec.js
+++ tests/button.spec.js
@@ -1,10 +1,11 @@
-const { test, expect } = require('@playwright/test');
-const path = require('path');
+  const { test, expect } = require('@playwright/test');
+  const path = require('path');
 
-test('button click shows new text', async ({ page }) => {
-  const filePath = 'file://' + path.resolve(__dirname, '../index.html');
-  await page.goto(filePath);
-  await page.click('#reveal-btn');
-  const message = await page.locator('#message');
-  await expect(message).toHaveText('Hello from CircleCI!');
-});
+  test('button click shows new text', async ({ page }) => {
+    const filePath = 'file://' + path.resolve(__dirname, '../index.html');
+    await page.goto(filePath);
+    await page.click('#reveal-btn');
+    const message = await page.locator('#message');
+    //await expect(message).toHaveText('Hello from CircleCI!');
+    await expect(message).toHaveText('This text does not exist');
+  });
